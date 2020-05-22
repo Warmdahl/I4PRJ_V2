96d5b343405aad77964d46810649098f5b66eb1c
Ikille4fun <Alexander.wh@hotmail.com>
2020-05-22 11:52:45 +0200

    Update RCCS.DatabaseUsers

--- RCCS/RCCS.DatabaseUsers/Data/RCCSUsersContext.cs
+++ RCCS/RCCS.DatabaseUsers/Data/RCCSUsersContext.cs
@@ -1,19 +1,40 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.EntityFrameworkCore.Metadata.Conventions;
-//using Microsoft.Extensions.Configuration;
-//using RCCS.DatabaseUsers.Model.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.Extensions.Configuration;
+using RCCS.DatabaseUsers.Model.Entities;
 
-//namespace RCCS.DatabaseUsers.Data
-//{
-//    class RCCSUsersContext
-//    {
-//        public RCCSUsersContext(DbContextOptions<RCCSUsersContext> options) : base(options) { }
+namespace RCCS.DatabaseUsers.Data
+{
+    class RCCSUsersContext : DbContext
+    {
+        public RCCSUsersContext(DbContextOptions<RCCSUsersContext> options)
+            : base(options)
+        {
+        }
 
-//        public DbSet<EfAdmin> efAdmins { get; set; }
-//        public DbSet<EfCoordinator> efCoordinators { get; set; }
-//        public DbSet<EfNursingStaff> efNursingStaffs { get; set; }
-//    }
-//}
+        public DbSet<EfUser> Users { get; set; }
+        public DbSet<EfAdmin> Admins { get; set; }
+        public DbSet<EfCoordinator> Coordinators { get; set; }
+        public DbSet<EfNursingStaff> NursingStaffs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure indexes
+            modelBuilder.Entity<EfUser>()
+                .HasIndex(p => p.PersonaleId)
+                .IsUnique();
+            modelBuilder.Entity<EfAdmin>()
+                .HasIndex(p => p.PersonaleId)
+                .IsUnique();
+            modelBuilder.Entity<EfCoordinator>()
+                .HasIndex(p => p.PersonaleId)
+                .IsUnique();
+            modelBuilder.Entity<EfNursingStaff>()
+                .HasIndex(p => p.PersonaleId)
+                .IsUnique();
+        }
+    }
+}
