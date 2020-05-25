2b2e7a23ddde5111e40f53db8886b862e125e7cc
Rene Street <20112809@post.au.dk>
2020-05-25 20:43:56 +0200

    Made GetCreateCitizin return ActionResult. Unit testet for GetCreateCitizin. Made test for CreateProgressReportController for posting of ProgressReport

--- RCCS/RCCS.DatabaseCitizenResidency.Unit.Tests/Tests/CreateProgressReportControllerUnitTests.cs
+++ RCCS/RCCS.DatabaseCitizenResidency.Unit.Tests/Tests/CreateProgressReportControllerUnitTests.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RCCS.DatabaseAPI.RCCSCitizenResidencyDbViewControllers;
 using RCCS.DatabaseCitizenResidency.Data;
+using RCCS.DatabaseCitizenResidency.Model;
 using RCCS.DatabaseCitizenResidency.Unit.Tests.Utilities;
 using RCCS.DatabaseCitizenResidency.ViewModel;
 using Xunit;
@@ -25,7 +26,7 @@ namespace RCCS.DatabaseCitizenResidency.Unit.Tests.Tests
         }
 
         [Fact]
-        public async void GetProgressReport_GetsProgressReportForCitizen()
+        public async void GetProgressReport_GetsProgressReportDataForCitizen()
         {
             using (var context = new RCCSContext(_contextOptions))
             {
@@ -45,6 +46,38 @@ namespace RCCS.DatabaseCitizenResidency.Unit.Tests.Tests
                 Assert.Equal("Citizen Citizensen", prvm.Name);
                 Assert.Equal("Kærgården", prvm.RespiteCareHomeName);
                 Assert.Equal(new DateTime(2021, 01, 29), prvm.PlannedDischargeDate);
+            }
+        }
+
+        [Fact]
+        public async void PostProgressReport_PostsProgressReport()
+        {
+            using (var context = new RCCSContext(_contextOptions))
+            {
+                //Arrange
+                var createProgressReportController = new CreateProgressReportController(context);
+                var createProgressViewModel = new CreateProgressReportViewModel
+                {
+                    CPR = 2905891233,
+                    Title = "Progressreport title",
+                    Report = "Test report",
+                    ResponsibleCaretaker = "Me"
+                };
+
+                //Act
+                var progressReportViewModel = await createProgressReportController.PostProgressReport(createProgressViewModel);
+
+                //Assert
+                Assert.NotNull(progressReportViewModel);
+                var actionResult = Assert.IsType<ActionResult<ProgressReport>>(progressReportViewModel);
+                var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(actionResult.Result);
+                Assert.Equal("PostProgressReport", createdAtActionResult.ActionName);
+                var progressReportCreated = Assert.IsType<ProgressReport>(createdAtActionResult.Value);
+
+                Assert.True(createProgressViewModel.CPR.Equals(progressReportCreated.CitizenCPR));
+                Assert.True(createProgressViewModel.Title.Equals(progressReportCreated.Title));
+                Assert.True(createProgressViewModel.Report.Equals(progressReportCreated.Report));
+                Assert.True(createProgressViewModel.ResponsibleCaretaker.Equals(progressReportCreated.ResponsibleCaretaker));
             }
         }
     }
